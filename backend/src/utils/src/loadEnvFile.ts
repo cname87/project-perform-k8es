f5692941f6fa7b5d1fbb77a57b5b63e6bef47be7
Seán A. Young <cname87@yahoo.com>
2020-05-04 19:59:34 +0100

    Checkpoint 3 - all working

--- backend/src/utils/src/loadEnvFile.ts
+++ backend/src/utils/src/loadEnvFile.ts
@@ -1,10 +1,10 @@
 /**
  * Utility to import the .env file into process.env.
  * This should be called as the first line to set configuration parameters before they might be needed.
- * The .env files must be called .envDevelopment, .envProduction & .envStaging, and must be in a directory pwd/backend.
+ * The .env files must be called .envDevelopment, .envProduction & .envStaging, and must be in a directory above this directory.
  * Which .env file imported is dependent on the value of process.env.NODE_ENV
  * Note that the GCP production server sets NODE_ENV to 'production', and the GCP Build configuration file sets NODE_ENV to 'staging', but otherwise it is undefined (unless otherwise set as a command line parameter, or otherwise set before this file is called).
- * If NODE_ENV === 'staging' then it is set to 'production' in this module;
+ * If NODE_ENV === 'staging' then it is set to 'production' after the staging .env file is loaded;
  * If NODE_ENV === 'production' (or 'staging') then key parameters are checked and warnings are printed if they are not set to match a final production set up.
  */
 import dotenv from 'dotenv';
