22d1191cbd87ce04180b6ee2f263e21bedb10b72
Seán A. Young <cname87@yahoo.com>
2020-07-03 18:29:54 +0100

    Delete un-needed files

--- frontend/e2e/src/pages/elements/progress-bar.elements.ts
+++
@@ -1,9 +0,0 @@
-import { by, element } from 'protractor';
-
-export function getProgressBarElements() {
-  const progressBar = element(by.css('mat-progress-bar'));
-
-  return {
-    progressBar,
-  };
-}
